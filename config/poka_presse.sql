d60ef1208960f4194a9b1ad133b4a1a0d9703c17
redlinka <nachnouchiadam@gmail.com>
2025-05-16 16:03:04 +0200

    fixed db file

--- config/poka_presse.sql
+++ config/poka_presse.sql
@@ -1,28 +1,28 @@
 CREATE TABLE Acteur(
-   matricule INT,
+   matricule SERIAL,
    nom VARCHAR(50),
    prenom VARCHAR(50),
    mail VARCHAR(50),
-   salarie LOGICAL,
+   salarie BOOLEAN NOT NULL,
    fonction CHAR(2),
    PRIMARY KEY(matricule)
 );
 
 CREATE TABLE Rubrique(
-   num_rubrique SMALLINT,
+   num_rubrique SERIAL,
    nom_rubrique VARCHAR(50),
-   num_rubrique_1 SMALLINT,
+   num_rubrique_ancetre SMALLINT,
    PRIMARY KEY(num_rubrique),
    FOREIGN KEY(num_rubrique_ancetre) REFERENCES Rubrique(num_rubrique)
 );
 
 CREATE TABLE Article(
-   num_article INT,
+   num_article SERIAL,
    titre VARCHAR(50),
    chapeau VARCHAR(50),
    lien_contenu VARCHAR(50),
    date_acceptation DATE,
-   publie LOGICAL NOT NULL,
+   publie BOOLEAN NOT NULL,
    nb_feuillets INT,
    num_rubrique SMALLINT NOT NULL,
    PRIMARY KEY(num_article),
@@ -30,15 +30,15 @@ CREATE TABLE Article(
 );
 
 CREATE TABLE Image(
-   num_image INT,
+   num_image SERIAL,
    lien_image VARCHAR(50),
    PRIMARY KEY(num_image),
    UNIQUE(lien_image)
 );
 
 CREATE TABLE Pigiste(
-   mat_pigiste INT,
-   notoriete CURRENCY,
+   mat_pigiste SERIAL,
+   notoriete DECIMAL(10, 2),
    PRIMARY KEY(mat_pigiste),
    FOREIGN KEY(mat_pigiste) REFERENCES Acteur(matricule)
 );
@@ -51,16 +51,16 @@ CREATE TABLE Pays(
 );
 
 CREATE TABLE Maquettiste(
-   mat_maquettiste INT,
+   mat_maquettiste SERIAL,
    PRIMARY KEY(mat_maquettiste),
    FOREIGN KEY(mat_maquettiste) REFERENCES Acteur(matricule)
 );
 
 CREATE TABLE Maquette(
-   num_vers INT,
+   num_vers SERIAL,
    date_creation DATE,
    lien_maquette VARCHAR(50),
-   mat_maquettiste INT NOT NULL,
+   mat_maquettiste SERIAL NOT NULL,
    PRIMARY KEY(num_vers),
    FOREIGN KEY(mat_maquettiste) REFERENCES Maquettiste(mat_maquettiste)
 );
@@ -68,7 +68,7 @@ CREATE TABLE Maquette(
 CREATE TABLE Numero(
    code VARCHAR(50),
    date_publication DATE,
-   num_vers INT NOT NULL,
+   num_vers SERIAL,
    PRIMARY KEY(code),
    FOREIGN KEY(num_vers) REFERENCES Maquette(num_vers)
 );
@@ -86,7 +86,7 @@ CREATE TABLE distribution(
 );
 
 CREATE TABLE contient(
-   num_rubrique SMALLINT,
+   num_rubrique SERIAL,
    code VARCHAR(50),
    PRIMARY KEY(num_rubrique, code),
    FOREIGN KEY(num_rubrique) REFERENCES Rubrique(num_rubrique),
@@ -94,24 +94,24 @@ CREATE TABLE contient(
 );
 
 CREATE TABLE ecriture(
-   num_article INT,
-   mat_pigiste INT,
+   num_article SERIAL,
+   mat_pigiste SERIAL,
    PRIMARY KEY(num_article, mat_pigiste),
    FOREIGN KEY(num_article) REFERENCES Article(num_article),
    FOREIGN KEY(mat_pigiste) REFERENCES Pigiste(mat_pigiste)
 );
 
 CREATE TABLE liaison(
-   num_article INT,
-   num_image INT,
+   num_article SERIAL,
+   num_image SERIAL,
    PRIMARY KEY(num_article, num_image),
    FOREIGN KEY(num_article) REFERENCES Article(num_article),
    FOREIGN KEY(num_image) REFERENCES Image(num_image)
 );
 
 CREATE TABLE apparition(
-   num_image INT,
-   num_vers INT,
+   num_image SERIAL,
+   num_vers SERIAL,
    PRIMARY KEY(num_image, num_vers),
    FOREIGN KEY(num_image) REFERENCES Image(num_image),
    FOREIGN KEY(num_vers) REFERENCES Maquette(num_vers)
